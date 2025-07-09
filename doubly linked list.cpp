b9aa72b2cac1e57611de6cd1ded1265c3cec55b1
ZizahSwiftie <azizahulnha@gmail.com>
2025-07-09 15:10:58 +0700

    Fungsi untuk membuat node baru

--- doubly linked list.cpp
+++ doubly linked list.cpp
@@ -9,3 +9,20 @@ struct Node {
 
 Node *head = NULL;
 Node *tail = NULL;
+
+void buatNodeBaru(int data) 
+{
+    Node *baru = new Node;
+    baru->data = data;
+    baru->next = NULL;
+    baru->prev = NULL;
+
+    if (head == NULL) {
+        head = baru;
+        tail = baru;
+    } else {
+        tail->next = baru;
+        baru->prev = tail;
+        tail = baru;
+    }
+}
